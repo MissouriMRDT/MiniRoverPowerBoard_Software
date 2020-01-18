c4f9c9697fe558cd2767fc27a2d8fd0c8a102f1d
Jacob Lipina <jrlwd5@mst.edu>
2020-01-18 00:42:21 -0600

    Adding BMS Functionalities

--- MiniRoverPowerBoard_Software/MiniRoverPowerBoard_Software.h
+++ MiniRoverPowerBoard_Software/MiniRoverPowerBoard_Software.h
@@ -34,6 +34,60 @@
 
 // Constants //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define 
+#define VCC						3300
+
+// ACS722LLCTR-40AU-T IC Sensor Specs 
+#define SENSOR_SENSITIVITY   	0.066    //volts/amp
+#define SENSOR_SCALE         	0.1      //volts/amp
+#define SENSOR_BIAS           	VCC * SENSOR_SCALE
+
+#define CURRENT_MAX           	40000//mA
+#define CURRENT_MIN           	0//mA
+#define OVERCURRENT           	35000 //mA
+#define CURRENT_ADC_MIN     	0 //bits
+#define CURRENT_ADC_MAX     	1024 //bits
+
+// Voltage Measurments
+#define PACK_VOLTS_MIN          0 //mV		//Lowest possible pack voltage value we could measure. 
+#define PACK_VOLTS_MAX        	8400 //mV	//Highest possible pack voltage value we could measure.
+#define PACK_UNDERVOLTAGE   	5400 //mV	//If pack voltage reaches this voltage, turn off rover and BMS suicide.
+#define PACK_LOWVOLTAGE     	6100 //mV	//If pack voltage reaches this voltage, notify of low voltage.
+#define PACK_SAFETY_LOW     	PACK_UNDERVOLTAGE - 3000 //mV	//Even though I shouldn't be need to measure a voltage lower than undervoltage 
+#define PACK_EFFECTIVE_ZERO   	1000 //mV 	//Pack voltage below this value is considered to be adc noise and not valid measurments. This is because the battery cannot physically be this low. So there must be an error with reading the voltage
+#define PACK_V_ADC_MIN      	0 //bits
+#define PACK_V_ADC_MAX      	4096 //bits
+
+// Delays
+#define DEBOUNCE_DELAY      	10 //msec		//After an overcurrent or undervoltage is detected, delay by this and measure the pin again to confirm whether error is occurring.
+#define RECHECK_DELAY    		10000 //msec	//Used to measure time since first overcurrent. If second overcurrent occurs within this delay time, BMS commits suicide.
+#define RESTART_DELAY     		5000 //msec		//Used after first overcurrent as delay before turning rover back on.
+#define LOGIC_SWITCH_REMINDER 	60000 //msec 	//Every cycle of this period of time, the buzzer notifys someone that logic switch was forgotten.
+#define IDLE_SHUTOFF_TIME   	600000 //msec or 10 minutes	//After this period of time passes, the BMS will commit suicide.
 
 // Function Declarations //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void getMainCurrent(int16_t &main_current);
+
+
+void reactOverCurrent()
+
+
+void notifyEstop(); //Buzzer sound: beeeeeeeeeeeeeeeeeeeep beeeeeeeeeep beeeeep beeep bep
+
+
+void notifyLogicSwitch(); //Buzzer sound: beeep beeep
+
+
+void notifyEstopReleased(); //Buzzer sound: beep
+
+
+void notifyReboot(); //Buzzer sound: beeeeeeeeeep beeep beeep
+
+
+void notifyOverCurrent(); //Buzzer Sound: beeeeeeeeeeeeeeeeeeeeeeeeeeeeeep
+
+
+void notifyUnderVoltage(); //Buzzer Sound: beeep beeep beeep beeep beeeeeeeeeeeeeeeeeeeep
+
+
+void notifyLowVoltage(); //Buzzer Sound: beeep beeep beeep
--- MiniRoverPowerBoard_Software/MiniRoverPowerBoard_Software.ino
+++ MiniRoverPowerBoard_Software/MiniRoverPowerBoard_Software.ino
@@ -11,34 +11,87 @@
 // Setup & Main Loop //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 uint16_t main_current, act_voltage, log_voltage;
+int num_loop = 0;
 
 void setup() 
 {
   Serial.begin(9600);
 
+  pinMode(PACK_IMEAS_PIN, INPUT);
+  pinMode(V_ACT_SENSE_PIN, INPUT);
+  pinMode(V_LOG_SENSE_PIN, INPUT);
+  pinMode(RIGHT_SW_PIN, INPUT);
+  pinMode(LEFT_SW_PIN, INPUT);
+
+  pinMode(BUZZER_EN_PIN, OUTPUT);
+  pinMode(ESTOP_EN_PIN, OUTPUT);
+  pinMode(LOG_SW_EN_PIN, OUTPUT);
+  pinMode(CONTROLLER_DAT_PIN, OUTPUT);
+  pinMode(CONTROLLER_ATT_PIN, OUTPUT);
+  pinMode(CONTROLLER_CMD_PIN, OUTPUT);
+  pinMode(CONTROLLER_CLK_PIN, OUTPUT);
+  pinMode(VNH_IN_A1_PIN, OUTPUT);
+  pinMode(VNH_IN_A2_PIN, OUTPUT);
+  pinMode(VNH_IN_B1_PIN, OUTPUT);
+  pinMode(VNH_IN_B2_PIN, OUTPUT);
+
+  Serial.println("Setup Complete.");
 }
 
 void loop() 
 {
-  
+  getMainCurrent(main_current);
+  reactOverCurrent();
 
+  getCellVoltage(cell_voltages);
+  reactUnderVoltage();
+  reactLowVoltage(cell_voltages);
+
+  getOutVoltage(pack_out_voltage);
+  reactEstopReleased();
+  reactForgottenLogicSwitch();
+
+  num_loop ++;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Static Variables for Below Functions /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //Current 
+static int num_overcurrent = 0;
+static bool overcurrent_state = false;
+static float time_of_overcurrent = 0;
+  
+  //Voltage
+static bool pack_undervoltage_state = false;
+static bool cell_undervoltage_state = false;
+static bool low_voltage_state = false;
+static int num_low_voltage_reminder = 0;
+static int time_of_low_voltage = 0;
+
+  //Logic Switch
+static bool forgotten_logic_switch = false;
+static int num_out_voltage_loops = 0;
+static int time_switch_forgotten = 0;
+static int time_switch_reminder = 0;
+static bool estop_released_beep = false;
+
+
+// Functions /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 void getMainCurrent(int16_t &main_current)
 {
-	//Serial.print("adc current:  ");
-	//Serial.println(analogRead(PACK_I_MEAS_PIN));
+	//Serial.print("adc main current: ");
+	//Serial.println(analogRead(PACK_IMEAS_PIN));
  
-  	main_current = ((map(analogRead(PACK_I_MEAS_PIN), CURRENT_ADC_MIN, CURRENT_ADC_MAX, CURRENT_MIN, CURRENT_MAX)*1069)/1000);//*950)/1000);
+  	main_current = map(analogRead(PACK_IMEAS_PIN), CURRENT_ADC_MIN, CURRENT_ADC_MAX, CURRENT_MIN, CURRENT_MAX); //*1069)/1000); //only scale if measurment too inacurate
 
-	//Serial.print("current:  ");
+	//Serial.print("Main current: ");
 	//Serial.println(main_current);
   	if(main_current > OVERCURRENT)
   	{
   	  delay(DEBOUNCE_DELAY);
-  	  if(map(analogRead(PACK_I_MEAS_PIN), CURRENT_ADC_MIN, CURRENT_ADC_MAX, CURRENT_MIN, CURRENT_MAX) > OVERCURRENT)
+  	  if(map(analogRead(PACK_IMEAS_PIN), CURRENT_ADC_MIN, CURRENT_ADC_MAX, CURRENT_MIN, CURRENT_MAX) > OVERCURRENT)
   	  {
   	    overcurrent_state = true;
 	
@@ -49,4 +102,219 @@ void getMainCurrent(int16_t &main_current)
   	  }//end if 
   	}//end if
   	return;
+}//end func
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void reactOverCurrent()
+{
+  if(overcurrent_state == true)
+  {
+    if(num_overcurrent == 0)
+    {
+      digitalWrite(ESTOP_EN_PIN, LOW);
+    
+      time_of_overcurrent = millis(); 
+      num_overcurrent++;
+    
+      notifyOverCurrent();
+    }//end if
+    else if(num_overcurrent == 1)
+    {
+      if(millis() >= (time_of_overcurrent + RESTART_DELAY))
+      {
+        digitalWrite(ESTOP_EN_PIN, HIGH);
+      }//end if
+      if(millis() >= (time_of_overcurrent + RESTART_DELAY + RECHECK_DELAY))
+      {
+        overcurrent_state = false;
+        num_overcurrent = 0;
+        time_of_overcurrent = 0;
+      }//end if
+    }//end else if
+    else
+    {
+      digitalWrite(ESTOP_EN_PIN, LOW);
+
+      notifyOverCurrent();
+
+      digitalWrite(LOG_SW_EN_PIN, HIGH); //Mini Suicide   
+    }//end else
+  }//end if
+  return;
+}//end func
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void notifyEstop() //Buzzer sound: beeeeeeeeeeeeeeeeeeeep beeeeeeeeeep beeeeep beeep bep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(500);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(100);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(25);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+
+  return;
+}//end func
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void notifyLogicSwitch() //Buzzer sound: beeep beeep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(100);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(100);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+
+  return;
+}//end func
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void notifyEstopReleased() //Buzzer sound: beep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(75);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+
+  return;
+}//end func
+
+void notifyReboot() //Buzzer sound: beeeeeeeeeep beeep beeep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(500);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(100);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(25);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(25);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(100);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(500);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  
+  return;
+}//end func
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void notifyOverCurrent() //Buzzer Sound: beeeeeeeeeeeeeeeeeeeeeeeeeeeeeep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(3000);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+
+  return;
+}//end func
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void notifyUnderVoltage() //Buzzer Sound: beeep beeep beeep beeep beeeeeeeeeeeeeeeeeeeep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(150);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(150);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(150);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(150);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(150);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(2000);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+
+  return;
+}//end func
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void notifyLowVoltage() //Buzzer Sound: beeep beeep beeep
+{
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(250);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+  delay(250);
+
+  digitalWrite(BUZZER_CTR_PIN, HIGH);
+  delay(250);
+  digitalWrite(BUZZER_CTR_PIN, LOW);
+
+  return;
 }//end func
