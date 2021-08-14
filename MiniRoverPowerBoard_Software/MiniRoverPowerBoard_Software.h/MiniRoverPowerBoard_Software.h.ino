5bbc7f545dc845670e1e847d060c5496bc754979
tman20 <31227414+tman20@users.noreply.github.com>
2021-08-13 23:27:53 -0500

    Update MiniRoverPowerBoard_Software.h.ino
    arduino moco shield and RC reciever decoder

--- MiniRoverPowerBoard_Software/MiniRoverPowerBoard_Software.h/MiniRoverPowerBoard_Software.h.ino
+++ MiniRoverPowerBoard_Software/MiniRoverPowerBoard_Software.h/MiniRoverPowerBoard_Software.h.ino
@@ -1,68 +1,121 @@
-#include <Servo.h>
+#include "DualVNH5019MotorShield.h"
 
-Servo Servo1;
 
-unsigned long int a,b,c;
-int x[15],ch1[15],ch[7],i;
-int mapval;
-//specifing arrays and variables to store values 
+DualVNH5019MotorShield md;
+
+
+unsigned long int a, b, c;
+int x[15], ch1[15], ch[7], i;
+int mot1;
+int mot2;
+int motorball1;
+int motorball2;
+int mot1save;
+int mot2save;
+int posdead = 30;
+int negdead = -30;
 
 void setup() {
-  Serial.begin(9600);
-  Servo1.attach(3);
-  pinMode(3,OUTPUT);
-  pinMode(2, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), read_me, FALLING);
-  // enabling interrupt at pin 2
+
+  Serial.begin(115200);
+  Serial.println("Dual VNH5019 Motor Shield");
+  md.init();
+
+
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(3), read_me, FALLING);
+
+  delay(1000);
+
 }
 
 void loop() {
-read_rc();
 
-Serial.print(ch[1]);Serial.print("\t");
-Serial.print(ch[2]);Serial.print("\t");
-Serial.print(ch[3]);Serial.print("\t");
-Serial.print(ch[4]);Serial.print("\t");
-Serial.print(ch[5]);Serial.print("\t");
-Serial.print(ch[6]);Serial.print("\n");
+  // read reciever
+  read_rc();
+
+  Serial.print(ch[1]); Serial.print("\t");
+  Serial.print(ch[2]); Serial.print("\t");
+  Serial.print(ch[3]); Serial.print("\t");
+  Serial.print(ch[4]); Serial.print("\t");
+  Serial.print(ch[5]); Serial.print("\t");
+  Serial.print(ch[6]); Serial.print("\n");
 
 
-mapval = map(ch[2],72,892,0,180);
+  mot1 = map(ch[2], 72, 892, -800, 800);
+  mot2 = map(ch[4], 88, 920, -800, 800);
+  Serial.print(mot1);
+  Serial.print("mot1  ");
+  Serial.print(mot2);
+  Serial.println("mot2  ");
 
-Serial.println(mapval);
-Serial.print("MAPVAL");
-Servo1.write(mapval);
+  //DELAY
+  delay(0);
 
-if (ch[2] < 380){
-  Serial.println("MOTOR 1 LOW");
-  //Serial.println(ch[2]);
-  //Serial.println(mapval);
-  //Servo1.write(mapval);
+
+
+  // motor 2
+  if (mot2 < posdead && mot2 > -20) {
+    md.setM2Speed(0);
+  }
+  if ( mot2 > posdead) {
+
+    md.setM2Speed(mot2);
+    Serial.print("M1 current: ");
+    Serial.println(md.getM1CurrentMilliamps());
+  }
+  if ( mot2 < negdead) {
+
+    md.setM2Speed(mot2);
+    Serial.print("M1 current: ");
+    Serial.println(md.getM1CurrentMilliamps());
+  }
+
+
+  // motor 1
+  if (mot1 < posdead && mot1 > -20) {
+    md.setM1Speed(0);
+  }
+  if ( mot1 > posdead) {
+
+    md.setM1Speed(mot1);
+    Serial.print("M1 current: ");
+    Serial.println(md.getM1CurrentMilliamps());
+  }
+  if ( mot1 < negdead) {
+
+    md.setM1Speed(mot1);
+    Serial.print("M1 current: ");
+    Serial.println(md.getM1CurrentMilliamps());
+  }
+
+
+
 }
-if (ch[2] > 520){
-  Serial.println("MOTOR 1 HIGH");
-}
-if (ch[4] < 380){
-  Serial.println("MOTOR 2 LOW");
-}
-if (ch[4] > 520){
-  Serial.println("MOTOR 2 HIGH");
-}
-delay(100);
-}
-
 
 void read_me()  {
- //this code reads value from RC reciever from PPM pin (Pin 2 or 3)
- //this code gives channel values from 0-1000 values 
- //    -: ABHILASH :-    //
-a=micros(); //store time value a when pin value falling
-c=a-b;      //calculating time inbetween two peaks
-b=a;        // 
-x[i]=c;     //storing 15 value in array
-i=i+1;       if(i==15){for(int j=0;j<15;j++) {ch1[j]=x[j];}
-             i=0;}}//copy store all values from temporary array another array after 15 reading  
-void read_rc(){
-int i,j,k=0;
-  for(k=14;k>-1;k--){if(ch1[k]>10000){j=k;}}  //detecting separation space 10000us in that another array                     
-  for(i=1;i<=6;i++){ch[i]=(ch1[i+j]-1000);}}     //assign 6 channel values after separation space
+  //this code reads value from RC reciever from PPM pin (Pin 2 or 3)
+  //this code gives channel values from 0-1000 values
+  //    -: ABHILASH :-    //
+  a = micros(); //store time value a when pin value falling
+  c = a - b;  //calculating time inbetween two peaks
+  b = a;      //
+  x[i] = c;   //storing 15 value in array
+  i = i + 1;       if (i == 15) {
+    for (int j = 0; j < 15; j++) {
+      ch1[j] = x[j];
+    }
+    i = 0;
+  }
+}//copy store all values from temporary array another array after 15 reading
+void read_rc() {
+  int t, j, k = 0;
+  for (k = 14; k > -1; k--) {
+    if (ch1[k] > 10000) {
+      j = k; //detecting separation space 10000us in that another array
+    }
+  }
+  for (t = 1; t <= 6; t++) {
+    ch[t] = (ch1[t + j] - 1000); //assign 6 channel values after separation space
+  }
+}
